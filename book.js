dc3583ee8db845ac04beadf22a5d1f494061d0d0
cjscrimmy <craigscrimgeour@yahoo.com>
2020-07-03 14:55:29 +0100

    refactor book.js to have a class and constructor layout

--- book.js
+++ book.js
@@ -1,7 +1,15 @@
-const Book = function (author, title, genre) {
-  this.author = author;
-  this.title = title;
-  this.genre = genre;
-};
+// const Book = function (author, title, genre) {
+//   this.author = author;
+//   this.title = title;
+//   this.genre = genre;
+// };
+
+class Book {
+  constructor(author, title, genre){
+    this.author = author;
+    this.title = title;
+    this.genre = genre;
+  }
+}
 
 module.exports = Book;
